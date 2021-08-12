1a4a1b1ae2ffefdb641d50e5f7e4e85b0795596e
DylanCharton <d.charton@codeur.online>
2021-08-12 14:01:12 +0200

    modification de la structure du html

---
+++ js/main.js
@@ -0,0 +1,43 @@
+function test($msg) {
+    alert($msg);
+}
+
+/* CONTROLES MENU */
+function toggleMenu(el) {
+
+    var prt = el.parentNode;
+    var ctrl = prt.children[1];
+    var qte = ctrl.children[0];
+    
+    if(prt.className=="elmenu active") {
+        prt.classList.remove('active');
+        ctrl.style.display = "none";
+        
+     }else{
+         prt.classList.add('active');
+         ctrl.style.display = "inline-block";
+         qte = ctrl.children[0].innerHTML = 1;
+    }
+}
+
+function addToBasket(el) {
+    var prt = el.parentNode;
+    var gprt = prt.parentNode;
+    var getQte = gprt.children[0].innerHTML;
+    var qte = new Number(getQte);
+    qte++;
+    gprt.children[0].innerHTML = qte;    ;
+}
+
+function removeFromBasket(el) {
+    var prt = el.parentNode;
+    var gprt = prt.parentNode;
+    var getQte = gprt.children[0].innerHTML;
+    var qte = new Number(getQte);
+    qte--;
+    if(qte == 0) {
+        toggleMenu(gprt);
+    }
+    gprt.children[0].innerHTML = qte;    ;
+
+}
